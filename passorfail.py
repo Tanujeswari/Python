e2085bfae611294d77b685ddd4ed5fe7b625eda4
Tanujeswari <125129809+Tanujeswari@users.noreply.github.com>
2023-10-11 21:12:18 +0530

    11-10-2023:A3.py

---
+++ passorfail.py
@@ -0,0 +1,10 @@
+sno=int(input('enter sno:'))
+sname=input('enter student name:')
+group=input('enter group name:')
+s1=int(input('enter maths marks:'))
+s2=int(input('enter physics marks:'))
+s3=int(input('enter chemistry marks:'))
+if(s1>=40 and s2>=40 and s3>=40):
+    print('Pass')
+else:
+    print('Fail')
